0907e5b56bc44053b738d66ff3f923c9f423b436
Vane Arapa <vanessaah.lu@gmail.com>
2023-12-18 22:38:21 -0500

    improve configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  router: {
+    base: '/template-web/',
+  },
   head: {
     title: 'business-school',
     htmlAttrs: {
